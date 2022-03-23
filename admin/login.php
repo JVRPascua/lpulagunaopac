c1f45febd00d389ef76cfafbf8a0077545ed8ba3
Vincent <pascuavincentjohn@gmail.com>
2022-03-23 16:12:40 +0800

    commented all of login.php to find error

--- admin/login.php
+++ admin/login.php
@@ -1,25 +1,25 @@
 <?php
-require_once('connect.php');
-session_start();
+//require_once('connect.php');
+//session_start();
 
-if(isset($_POST['login'])){ 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+//if(isset($_POST['login'])){ 
+//    $username = $_POST['username'];
+//    $password = $_POST['password'];
 
 
-    $login_query = "SELECT * from admin_tbl WHERE username='$username' AND pass='$password' ";
+ //   $login_query = "SELECT * from admin_tbl WHERE username='$username' AND pass='$password' ";
 
-    $result = @mysqli_query($dbc,$login_query);
+ //   $result = @mysqli_query($dbc,$login_query);
 
-    if(mysqli_num_rows($result)){
-        echo '<script>window.location.href = "welcome.php";</script>';  
-        $_SESSION['login_user'] = $username;
-    }
-    else{
-        echo '<script>alert("Email or Password is incorrect!");
-        window.location.href = "loginpage.html";</script>';  
-    }
+ //   if(mysqli_num_rows($result)){
+  //      echo '<script>window.location.href = "welcome.php";</script>';  
+   //     $_SESSION['login_user'] = $username;
+   // }
+   // else{
+    //    echo '<script>alert("Email or Password is incorrect!");
+   //     window.location.href = "loginpage.html";</script>';  
+   // }
 
-     }
+   //  }
 
 ?>
