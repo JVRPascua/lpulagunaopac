a27e5b95110ebb044bbb53bd89828adb957fe26d
Vincent <pascuavincentjohn@gmail.com>
2022-03-30 01:36:28 +0800

    ReDeploy

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
-//define named constants
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_USER', 'root');
-DEFINE('DB_PASSWORD', '');
-DEFINE('DB_NAME', 'opac');
-
-//Establish connection
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR exit('Could not connect to MYSQL: ' .mysqli_connect_error());
-
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$dbc = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
